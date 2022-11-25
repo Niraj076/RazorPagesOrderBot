da3a7363ddf7ed6a7cf64c1e2f463b5b041a9ff3
Niraj076 <parmarniraj76@gmail.com>
2022-11-24 22:26:29 -0500

    Automated unit test

--- OrderBot.tests/OrderBotTest.cs
+++ OrderBot.tests/OrderBotTest.cs
@@ -20,27 +20,27 @@ namespace OrderBot.tests
             Assert.True(sInput.Contains("Welcome"));
         }
         [Fact]
-        public void TestShawarama()
+        public void TestDental()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[0];
-            Assert.True(sInput.ToLower().Contains("shawarama"));
+            Assert.True(sInput.ToLower().Contains("Dental"));
         }
         [Fact]
-        public void TestSize()
+        public void TestDate()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[1];
-            Assert.True(sInput.ToLower().Contains("size"));
+            Assert.True(sInput.ToLower().Contains("date"));
         }
         [Fact]
-        public void TestLarge()
+        public void TestCleaning()
         {
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            String sInput = oSession.OnMessage("large")[0];
-            Assert.True(sInput.ToLower().Contains("protein"));
-            Assert.True(sInput.ToLower().Contains("large"));
+            String sInput = oSession.OnMessage("cleaning")[0];
+            Assert.True(sInput.ToLower().Contains("appointment"));
+            
         }
         [Fact]
         public void TestChicken()
